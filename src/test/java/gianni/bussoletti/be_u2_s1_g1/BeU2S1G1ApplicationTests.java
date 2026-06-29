30b453eb780eab56c64efe9b25d4a3627315f1fc
giannibussoletti <bussolettigianni@gmail.com>
2026-06-29 12:52:55 +0200

    Initial commit

---
+++ src/test/java/gianni/bussoletti/be_u2_s1_g1/BeU2S1G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package gianni.bussoletti.be_u2_s1_g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2S1G1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
